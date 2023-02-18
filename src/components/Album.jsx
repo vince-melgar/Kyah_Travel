a1af41db81231228319be1106a1bd8ee60e56764
vince-melgar <118233473+vince-melgar@users.noreply.github.com>
2023-02-19 05:22:11 +0800

    Lightbox

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,3 +1,6 @@
+import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+
 import Img1 from "../images/gallery-1.jpg";
 import Img2 from "../images/gallery-2.jpg";
 import Img3 from "../images/gallery-3.jpg";
@@ -8,60 +11,69 @@ import Img7 from "../images/gallery-7.jpg";
 import Img8 from "../images/gallery-8.jpg";
 import Img9 from "../images/gallery-9.jpg";
 
-
 const Album = () => {
-    let data = [
-      {
-        id: 1,
-        imgSrc: Img1,
-      },
-      {
-        id: 2,
-        imgSrc: Img2,
-      },
-      {
-        id: 3,
-        imgSrc: Img3,
-      },
-      {
-        id: 4,
-        imgSrc: Img4,
-      },
-      {
-        id: 5,
-        imgSrc: Img5,
-      },
-      {
-        id: 6,
-        imgSrc: Img6,
-      },
-      {
-        id: 7,
-        imgSrc: Img7,
-      },
-      {
-        id: 8,
-        imgSrc: Img8,
-      },
-      {
-        id: 9, 
-        imgSrc: Img9,
-      },
-    ]
+  let data = [
+    {
+      id: 1,
+      imgSrc: Img1,
+    },
+    {
+      id: 2,
+      imgSrc: Img2,
+    },
+    {
+      id: 3,
+      imgSrc: Img3,
+    },
+    {
+      id: 4,
+      imgSrc: Img4,
+    },
+    {
+      id: 5,
+      imgSrc: Img5,
+    },
+    {
+      id: 6,
+      imgSrc: Img6,
+    },
+    {
+      id: 7,
+      imgSrc: Img7,
+    },
+    {
+      id: 8,
+      imgSrc: Img8,
+    },
+    {
+      id: 9, 
+      imgSrc: Img9,
+    }, 
+  ];
+  
+  const [model, setModel] = useState(false);
+  const [tempimgSrc, setTempImgSrc] = useState('');
 
-    return(
-        <>
-            <div className="gallery">
-                {data.map((item, index)=>{
-                    return(
-                        <div className="pics" key={index}>
-                            <img src={item.imgSrc} style={{width: '100%'}} />
-                        </div>
-                    )
-                })}
-            </div>
-        </>
-    )  
-  }
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
 
-  export default Album
+  return (
+    <>
+      <div className={model ? "model open" : "model"}>
+        <img src={tempimgSrc} />
+            <CloseIcon onClick={() => setModel(false)} />
+      </div>
+      <div className="gallery">
+        {data.map((item, index ) => (
+          <div className="pics" key={item.id} onClick={() => getImg(item.imgSrc)}>
+            <img src={item.imgSrc} style={{ width: '100%' }} alt={`gallery-${item.id}`} />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+};
+
+export default Album;
