bad2955cb515cc094bf87914c5a1a75e58c82894
vince-melgar <118233473+vince-melgar@users.noreply.github.com>
2023-02-20 16:20:05 +0800

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ if (isLoading) {
                             </li>
                     </ul>
                          <form class="d-flex"> 
-                                { isAuthenticated ? <button className="btn btn-link nav-link " onClick={() => logout({ logoutParams: { returnTo: window.location.origin}})}>Log Out</button> : <button className="btn btn-link nav-link " onClick={() => loginWithRedirect()}>Log In</button>}                               
+                                { isAuthenticated ? <button className="btn btn-link nav-link btn-outline-success" onClick={() => logout({ logoutParams: { returnTo: window.location.origin}})}>Log Out</button> : <button className="btn btn-link nav-link btn-outline-success " onClick={() => loginWithRedirect()}>Log In</button>}                               
                                 {/* <button onClick={() => loginWithRedirect()} class="btn btn-outline-success" style={{ padding: "10px", margin: "0 15px" }} >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
