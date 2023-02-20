8466eece13123ae1c4efbe45cabc7d3b7491af30
vince-melgar <118233473+vince-melgar@users.noreply.github.com>
2023-02-21 03:18:38 +0800

    Revert "added modal"
    This reverts commit 334a79a9c05220db11312e394374fb863be1a42b.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import HeaderForm from "./header/HeaderForm";
+import React from 'react'
 
 function Header() {
   return (
     <div>
-
       <header className="flex">
         <div className="container">
           <div className="header-title">
@@ -62,30 +60,11 @@ function Header() {
                       Submit and book
                     </button>
                   </div>
-
-        <header className = "flex">
-            <div className = "container">
-                <div className = "header-title">                
-                    <h1>Do you have an interest in discovering new places?</h1>
-                    <p>Bring your search to a close! Your next journey has countless possibilities. From stunning sights to invigorating experiences, we have it all. Don't delay! Start exploring today and make memories that will last forever. Book now!</p>
                 </div>
-                <div className = "header-form">
-                    <h2>Choose your Travel location:</h2>
-                    <form className = "flex">
-                        <input type = "text" className = "form-control" placeholder="Destination name"/>
-                        <input type="date" className = "form-control" placeholder="Date"/>
-                        <input type="number" className = "form-control" placeholder="Price (Php)"/>
-                        <input type="submit" className = "btn" value = "Search"/>
-                    </form>
-
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </header>
+        </header>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
